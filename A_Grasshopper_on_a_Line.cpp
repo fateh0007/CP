81bbbc71fedc653eabd4776863b143435151f0ed
fateh0007 <fatehalam123463@gmail.com>
2026-02-16 16:26:12 +0530

    Grasshopper on  aline

---
+++ A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <map> 
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>  
+#include <numeric>
+using namespace std;
+#define int long long
+const int mod = 1e9 + 7;
+const int INF = LLONG_MAX;
+
+void solve(){
+    int x,k;
+    cin>>x>>k;
+    if(x%k!=0){
+        cout<<1<<endl;
+        cout<<x<<endl;
+    }else{
+        cout<<2<<endl;
+        cout<<x-1<<" "<<1<<endl;
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
