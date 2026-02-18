c68b2ccd8c9a3ca0f9ab77fa90b11b7e88f97dc3
fateh0007 <fatehalam123463@gmail.com>
2026-02-18 18:47:11 +0530

    Done Weird Sum

--- A_Weird_Sum.cpp
+++ A_Weird_Sum.cpp
@@ -14,15 +14,44 @@ using namespace std;
 #define int long long
 const int mod = 1e9 + 7;
 const int INF = LLONG_MAX;
-
+int calc(vector<int> &v){
+    sort(v.begin(),v.end());
+    int pref = 0;
+    int res = 0;
+    for(int i=0;i<v.size();i++){
+        res+= v[i]*i - pref;
+        pref+=v[i];
+    }
+    return res;
+}
 void solve(){
-    
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int> > a(n,vector<int>(m));
+    map<int,vector<pair<int,int> > > mp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+            mp[a[i][j]].push_back(make_pair(i,j));
+        }
+    }
+    int sum = 0;
+    for(auto it: mp){
+        vector<int> rows,cols;
+        for(auto &p:it.second){
+            rows.push_back(p.first);
+            cols.push_back(p.second);
+        }
+        sum+= calc(rows);
+        sum+=calc(cols);
+    }
+    cout<<sum<<endl;
 }
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin>>t;
+    int t=1;
+    // cin>>t;
     while(t--) solve();
 }
