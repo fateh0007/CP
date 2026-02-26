65ceb8c703d9aa06287e0426c5f785a67c778850
fateh0007 <fatehalam123463@gmail.com>
2026-02-26 22:19:08 +0530

    Mashmokh and ACM

---
+++ B_Mashmokh_and_ACM.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <map> 
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>  
+#include <numeric>
+using namespace std;
+#define int long long
+const int mod = 1e9 + 7;
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    // cin>>t;
+    while(t--) solve();
+}
