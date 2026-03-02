0d569adfd74c048b44f90471f8280aa113d163e8
fateh0007 <fatehalam123463@gmail.com>
2026-03-02 09:37:49 +0530

    Started 1500 rated questions on codeforces

---
+++ F_Greetings.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <map> 
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>  
+#include <numeric>
+using namespace std;
+#define int long long
+const int mod = 1e9 + 7;
+int countAndMerge(vector<int>& arr,int l,int m,int r){
+    int n1=m-l+1,n2=r-m;
+    vector<int> left(n1),right(n2);
+    for(int i=0;i<n1;i++)
+        left[i]=arr[i+l];
+    for(int j=0;j<n2;j++)
+        right[j]=arr[m+1+j];
+    int res=0;
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(left[i]<=right[j])
+            arr[k++]=left[i++];
+        else{
+            arr[k++]=right[j++];
+            res+=(n1-i);
+        }
+    }
+    while(i<n1)
+        arr[k++]=left[i++];
+    while(j<n2)
+        arr[k++]=right[j++];
+    return res;
+}
+int countInv(vector<int>& arr,int l,int r){
+    int res=0;
+    if(l<r){
+        int m=(r+l)/2;
+        res+=countInv(arr,l,m);
+        res+=countInv(arr,m+1,r);
+        res+=countAndMerge(arr,l,m,r);
+    }
+    return res;
+}
+
+int inversionCount(vector<int> &arr) {
+  	int n = arr.size();
+  	return countInv(arr, 0, n-1);
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<pair<int,int> >a;
+    for(int i=0;i<n;i++){
+        int u,v;
+        cin>>u>>v;
+        a.push_back(make_pair(v,u));
+    }
+    sort(a.begin(),a.end());
+    vector<int> temp;
+    for(int i=0;i<n;i++){
+        temp.push_back(a[i].second);
+    }
+    int ans = inversionCount(temp);
+    cout<<ans<<endl;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
