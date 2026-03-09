5a77844f3fdac4c5f32d9a4ebf3cb0a2aaf9c2cf
fateh0007 <fatehalam123463@gmail.com>
2026-03-09 19:17:08 +0530

    Completed Balanced BitString

--- A_Balanced_Bitstring.cpp
+++ A_Balanced_Bitstring.cpp
@@ -19,7 +19,32 @@ void solve(){
     cin>>n>>k;
     string s;
     cin>>s;
-    
+    for(int i=k;i<n;i++){
+        if(s[i]=='1'){
+            if(s[i%k]=='0'){
+                cout<<"NO"<<endl;
+                return;
+            }
+            s[i%k] = '1';
+        }
+        else if(s[i]=='0'){
+            if(s[i%k]=='1'){
+                cout<<"NO"<<endl;
+                return;
+            }
+            s[i%k] = '0';
+        }
+    }
+    int c1 = 0,c0 = 0;
+    for(int i=0;i<k;i++){
+        if(s[i]=='1') c1++;
+        if(s[i]=='0') c0++;
+    }
+    if(c1>k/2 || c0 > k/2){
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
 }
 
 signed main(){
